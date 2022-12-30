d27ab923cb02537fde62e5d3098f46faa7dd3e8d
AnthonyJRay <ajrayid@gmail.com>
2022-12-29 18:30:50 -0600

    Refactored state in expenses. Added rough responsive styles. Add expense button adds new blank expense item.

--- src/components/expenses/ExpensesBody.jsx
+++ src/components/expenses/ExpensesBody.jsx
@@ -1,28 +1,51 @@
+import { useState } from 'react'
+
 import {
   ListItem,
   Box,
   Text,
   Button,
-} from "@chakra-ui/react";
+  Input
+} from "@chakra-ui/react"
 
 import { CloseIcon, CheckIcon, EditIcon } from "@chakra-ui/icons"
 
 export default function ExpensesBody(props) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editInput, setEditInput] = useState("");
+
+  const {
+    expenseDate, 
+    expenseName, 
+    expenseTotal, 
+    expenseDesc
+  } = props.expenseItems;
+
+  function editClickHandler() {
+    setIsEditing(!isEditing)
+    if(!isEditing) {
+      console.log("Editing!")
+    } else {
+      console.log("Saved!")
+    }
+  }
+
+  console.log(props)
   return (
-    <ListItem display={"flex"} alignItems={"center"} p={1} m={2} border={"1px"} borderRadius={"5px"} borderColor={"gray.500"}>
-          <Box display="flex" flexDirection={"row"} justifyContent={"space-around"} alignItems={"center"} w={"82%"}>
-            <Text p={2}>03/22/22</Text>
-            <Text p={2}>Gas</Text>
-            <Text p={3} color={"red"}>120.00</Text>
-            <Text w={"50%"} pl={1} pr={1}>Fuel for work truck</Text>
-          </Box>
-          <Box display={"flex"} justifyContent={"space-around"} w={"18%"}>
-            {props.isEditing ? 
-              <Button colorScheme={"green"}><CheckIcon color={"white"} onClick={props.editClickHandler} /></Button> : 
-              <Button colorScheme={"yellow"}><EditIcon color={"white"} onClick={props.editClickHandler} /></Button>
-            }
-            <Button colorScheme={"red"}><CloseIcon /></Button>
-          </Box>
-        </ListItem>
+    <ListItem display={"flex"} flexDirection={["column", null, "row", "row"]} alignItems={"center"} p={1} m={[10, 8, 6, 4, 2]} border={"1px"} borderRadius={"5px"} borderColor={"gray.500"}>
+      <Box display="flex" flexDirection={["column", null, "row", "row", "row"]} justifyContent={"space-around"} alignItems={"flex-start"} w={["90%"]}>
+        {isEditing ? <Input placeholder={"Expense Date"} m={2}/> : <Text p={2}>{expenseDate}</Text>}
+        {isEditing ? <Input placeholder={"Expense Name"} m={2}/> : <Text p={2}>{expenseName}</Text>}
+        {isEditing ? <Input placeholder={"Expense Total"} m={2}/> : <Text p={2} color={"red"}>{expenseTotal}</Text>}
+        {isEditing ? <Input placeholder={"Expense Description"} m={2}/> : <Text p={2}>{expenseDesc}</Text>}
+      </Box>
+      <Box display={"flex"} justifyContent={"space-around"} w={["60%", "36%", "24%", "26%", "20%", "10%"]} p={2} mt={4}>
+        {isEditing ? 
+          <Button colorScheme={"green"} onClick={editClickHandler} ><CheckIcon color={"white"} boxSize={4}/></Button> : 
+          <Button colorScheme={"yellow"} onClick={editClickHandler}><EditIcon color={"white"} boxSize={4}/></Button>
+        }
+        <Button colorScheme={"red"}><CloseIcon boxSize={3}/></Button>
+      </Box>
+    </ListItem>
   );
 }
--- src/pages/Expenses.jsx
+++ src/pages/Expenses.jsx
@@ -7,19 +7,19 @@ import {
   Heading,
   List,
   Button,
-  Text
+
 } from "@chakra-ui/react"
 
 import {
-  AddIcon
+  PlusSquareIcon
 } from "@chakra-ui/icons"
 
 export default function Expenses() {
   const [ expenseItems, setExpenseItem ] = useState([{
-    expenseDate: "",
-    expenseName: "",
-    expenseTotal: "",
-    expenseDesc: ""
+    expenseDate: "03/22/22",
+    expenseName: "Gas",
+    expenseTotal: "120.00",
+    expenseDesc: "Fuel for work truck"
   }])
 
   const [ newExpense, setNewExpense ] = useState({
@@ -29,21 +29,14 @@ export default function Expenses() {
     expenseDesc: ""
   })
 
-  const [isEditing, setIsEditing ] = useState(false);
-
-  function editClickHandler() {
-    setIsEditing(!isEditing)
-    if(!isEditing) {
-      console.log("Editing!")
-    } else {
-      console.log("Saved!")
-    }
-  }
   function deleteClickHandler() {
     return console.log("Expenses Delete Button!")
   }
   function addClickHandler() {
-    return console.log("New Expenses Added!")
+    return setExpenseItem([
+      ...expenseItems,
+      newExpense
+    ])
   }
 
 
@@ -51,12 +44,15 @@ export default function Expenses() {
     <Box textAlign={"center"} pt={5}>
       <Heading>Expenses</Heading>
       <List pt={5}>
-        <ExpensesBody isEditing={isEditing} expenseItems={expenseItems} editClickHandler={editClickHandler} deleteClickHandler={deleteClickHandler}/>
+        {expenseItems.map((expense, id) => {
+          return <ExpensesBody key={id} expenseItems={expenseItems[id]} deleteClickHandler={deleteClickHandler}/>
+        })}
+        {/* <ExpensesBody isEditing={isEditing} expenseItems={expenseItems} editClickHandler={editClickHandler} deleteClickHandler={deleteClickHandler}/> */}
       </List>
 
-      <Button colorScheme={"green"} w={"75%"} m={5} onClick={addClickHandler}>
-        <AddIcon/>
-        <Text w={"35%"}>Add New Expense</Text>
+      <Button colorScheme={"green"} w={["75%", null, "40%", "25"]} m={5} onClick={addClickHandler}>
+            <PlusSquareIcon fontSize={32}/>
+            <Box w={"45%"}>Add New Expense</Box>
       </Button>
     </Box>
 )
